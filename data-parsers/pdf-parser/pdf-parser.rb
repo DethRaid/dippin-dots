228ffaaa2394e1f6d5ae693e19e00f6d89daa5cf
David Dubois <dexcelstraun7@gmail.com>
2015-11-21 11:58:06 -0800

    Parse one txt file pretty good

--- data-parsers/pdf-parser/pdf-parser.rb
+++ data-parsers/pdf-parser/pdf-parser.rb
@@ -2,45 +2,52 @@ require 'rubygems'
 require_relative 'Course'
 
 def make_course( text )
+	puts "Making course from " +text +"\n"
 	lines = text.split( /\n/ )
 	course_id = lines[0]
 	course_name = lines[1]
 	course_desc = lines[2..-1].join( "\n" ).gsub "\n", ' '
-	semesteres_offered = lines[-1][/\(([A-Za-z, ]*)\)/][1..-2]
 
-	puts "\n"
-	puts "course_id: " +course_id
-	puts "course_name: " +course_name
-	puts "course_desc: " +course_desc
-	puts "semesteres_offered: " +semesteres_offered
+	if lines[-1] =~ /\(([A-Za-z, ]*)\)/
+		semesteres_offered = lines[-1][/\(([A-Za-z, ]*)\)/][1..-2]
+	end
 
 	Course.new( course_id, course_name, course_desc, nil, nil, nil, semesteres_offered )
 end
 
 def main
+	name1 = ARGV[0]
+	name2 = ARGV[1]
+	puts "Diffing files " +name1 +" and " +name2
+	
+	file_1_courses = gen_course_list_from_file name1
+	file_2_courses = gen_course_list_from_file name2
+end
+
+def gen_course_list_from_file( filename )
 	courses = Array.new
-	# Read in one PDF file
-	File.open( "../../data/txt/basic_course.txt" ) do |f|
+	File.open( "../../data/txt/2015_Undergrad_Course_Descriptions.txt" ) do |f|
 		course_accum = ""
-		should_end = 0
+		parsing_class = false
 		f.each_line do |line|
-			puts "course number " +should_end.to_s
 			# if the line has four capital letters followed by a dash and three numbers, we're starting a new course description
-			if line =~ /^[A-Z]{4}-[0-9]{3}$/
+			if line =~ /^[A-Z]{4}-[0-9]{3}/ and line.length < 15
 
-				courses << (make_course course_accum)
+				courses << (make_course course_accum) if course_accum.length > 1
 				course_accum = ""
+				parsing_class = true
+			
+			elsif line.length < 2
+				# If we find an empty line, stop parsing classes
+				parsing_class = false
 
-				return if should_end == 3
-				
-				should_end += 1
 			end
-			course_accum += line
+			course_accum += line if parsing_class
 
 		end
 		courses << (make_course course_accum)
 	end
-	puts courses[0].to_str
+	courses
 end
 
 main
