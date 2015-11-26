a63a7c62fd94b3572ea8ad74243cbc74b10ec484
David Dubois <david_dubois@intuit.com>
2015-11-25 17:06:36 -0800

    Added some data

---
+++ data-parsers/pdf-parser/run.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+ruby pdf_parser.rb ../../data/txt/2015_Undergrad_Course_Descriptions.txt ../../data/txt/2014_Undergrad_Course_Descriptions.txt
+ruby pdf_parser.rb ../../data/txt/2014_Undergrad_Course_Descriptions.txt ../../data/txt/2013_Undergrad_Course_Descriptions.txt
+ruby pdf_parser.rb ../../data/txt/2013_Undergrad_Course_Descriptions.txt ../../data/txt/2012_Undergrad_Course_Descriptions.txt
+ruby pdf_parser.rb ../../data/txt/2012_Undergrad_Course_Descriptions.txt ../../data/txt/2011_Undergrad_Course_Descriptions.txt
+
