a1350e0512cb527c550630a2e702d2ce8b5b7bac
David Dubois <dexcelstraun7@gmail.com>
2015-11-28 17:58:12 -0800

    Search button calls search_controller#create

--- app/app/controllers/poc_controller.rb
+++ app/app/controllers/poc_controller.rb
@@ -2,4 +2,8 @@ class PocController < ApplicationController
   def index
     
   end
+
+  def create
+    puts params
+  end
 end
--- app/app/controllers/search_controller.rb
+++ app/app/controllers/search_controller.rb
@@ -1,2 +1,5 @@
 class SearchController < ApplicationController
+  def create
+    puts params
+  end
 end
