290adcd7041e73d55f555b92c0e4e28c78919a5b
David Dubois <dexcelstraun7@gmail.com>
2015-11-28 19:39:22 -0800

    Can search classes by title

--- app/app/controllers/search_controller.rb
+++ app/app/controllers/search_controller.rb
@@ -3,11 +3,11 @@ class SearchController < ApplicationController
     # V1: grab all courses with the given ID
     # V2: grab all courses with the given ID -or- grab all courses with a
     #   fuzzy-search match with the search term, sorted by relevance
-    @courses = get_courses_for_search_term(params)
+    @courses = get_courses_for_search_term(params[:search])
   end
 
   private
   def get_courses_for_search_term(search_term)
-    [Course.new({:title => 'Course title', :description => 'Course description'})]
+    [Course.where(title: search_term).take]
   end
 end
--- app/db/seeds.rb
+++ app/db/seeds.rb
@@ -1,7 +1,19 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rake db:seed (or created alongside the db with db:setup).
+# This file should contain all the record creation needed to seed the database with
+# its default values.
+# The data can then be loaded with the rake db:seed (or created alongside the db with 
+# :setup).
 #
 # Examples:
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+
+# Create initial test data
+# TODO: hook into the data loaders and load real data
+courses = Course.create([
+    {:title => 'SWEN-261', :description => 'The worst'},
+    {:title => 'CSCI-240', :description => 'CS Theory'},
+    {:title => 'SWEN-262', :description => 'Refactoring'},
+    {:title => 'MLAS-301', :description => 'Intermediate ASL 1'},
+    {:title => 'CSMP-250', :description => 'Assembly'},
+    ])
